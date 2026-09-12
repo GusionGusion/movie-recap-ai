eae8eff65e207b165a3fde5f15ff513b5f402686
GusionGusion <44655709+GusionGusion@users.noreply.github.com>
2026-09-12 13:58:13 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1695,20 +1695,17 @@ if blur_enabled:
             # ---------------------------------------------
 
             canvas_result = st_canvas(
-                fill_color="rgba(255,0,0,0.20)",
-                stroke_width=4,
-                stroke_color="#FF0000",
-                background_image=preview_frame,
-                background_image_fit="stretch",
-                update_streamlit=True,
-                height=1024,
-                width=576,
-                drawing_mode="rect",
-                initial_drawing=initial_drawing,
-                display_toolbar=True,
-                max_display_height=700,
-                key=canvas_key
-            )
+    fill_color="rgba(255,0,0,0.20)",
+    stroke_width=4,
+    stroke_color="#FF0000",
+    background_image=preview_frame,
+    update_streamlit=True,
+    height=1024,
+    width=576,
+    drawing_mode="rect",
+    initial_drawing=initial_drawing,
+    key=canvas_key,
+)
 
             # ---------------------------------------------
             # READ OBJECT
